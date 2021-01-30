839c3b1151fce98c58eeefa99527227e96bfc8ca
nikhilleo <ramrakhyani94@gmail.com>
2021-01-30 21:15:19 +0530

    Message Timestamp Added

--- src/Message.js
+++ src/Message.js
@@ -3,14 +3,14 @@ import "./Message.css"
 import { Avatar } from '@material-ui/core'
 function Message({user,message,timestamp}) {
     // console.log(user);
-    // console.log(new Date(timestamp.toDate()).toUTCString()) ;
+    console.log(new Date(timestamp.toDate()).toUTCString()) ;
     return (
         <div className="message">
             <Avatar src={user?.photo} />
             
             <div className="message__info">
                 <h4>{user?.name}
-                    <span className="message__timestamp">{new Date(timestamp?.toDate()).toISOString}</span>
+                    <span className="message__timestamp">{new Date(timestamp?.toDate()).toLocaleString()}</span>
                 </h4>
                 <p>{message}</p>
             </div>
